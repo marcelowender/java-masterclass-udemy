987763cfc7bf6308b8fae2f4259dc0ec1993ae65
Marcelo Coelho <marcelowender@hotmail.com>
2020-04-20 22:41:03 -0300

    Moved

--- section4-exercises/src/br/com/marcelo/EqualSumChecker.java
+++
@@ -1,9 +0,0 @@
-package br.com.marcelo;
-
-public class EqualSumChecker {
-
-    public static boolean hasEqualSum(int firstNumber, int secondNumber, int sum) {
-        return firstNumber + secondNumber == sum;
-    }
-
-}
