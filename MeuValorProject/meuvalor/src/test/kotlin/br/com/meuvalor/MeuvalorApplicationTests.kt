dc9c92eac93d8398a5af9004b173bd739f608165
Marcelo Coelho <marcelowender@hotmail.com>
2020-07-19 02:04:41 -0300

    First commit Kotlin project

---
+++ MeuValorProject/meuvalor/src/test/kotlin/br/com/meuvalor/MeuvalorApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.meuvalor
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MeuvalorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
